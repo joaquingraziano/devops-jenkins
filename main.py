f9061cf6030c9da6aab41de5a20b33b225c442e0
Joaquin Graziano <jgraziano@altec.com.ar>
2023-03-16 15:23:06 -0300

    edito app

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 import time
 
-mensaje = "¡Voy a enviar este mensaje cada 1 segundo. Si me cuelgo sera tu culpa - test!2"
+mensaje = "¡Voy a enviar este mensaje cada 1 segundo. Si me cuelgo sera tu culpa !!"
 while True:
     print(mensaje)
     time.sleep(1)
